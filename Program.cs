985cc2c7545fabc4c642b77b320e8617a69d8c76
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-13 17:13:08 +0530

    Create a stack

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.ListDisplay();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        public void ListDisplay()
+        {
+            List<int> list = new List<int>();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+
+            foreach (int ele in list)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
